e7fbf0dffca96344a4c93ef80c115404d496a5f1
nickchuchunov <nick.chuchunov@yandex.u>
2021-04-20 19:49:28 +0300

    парктическое задание 1

---
+++ CNN-AlgorithmsTask1/CNN-AlgorithmsTask1/DefiningSimpleComplexNumber.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CNN_AlgorithmsTask1
+{
+
+
+   public class DefiningSimpleComplexNumber
+    {
+
+
+       public int d = 0;
+        public int i = 2;
+        public int z = 0;
+
+        public void DefiningSimpleComplexNumber1(double number)
+        {
+            double number1 = 0; // вспомогательная переменная для выполнения функции 1 раз для каждого number
+
+            while ( number1!=number) // while для возврата переменнх i и d с помощью оператора continue
+            {
+                if (i < number) { if (number % i == 0) { d++; continue; } else { i++; continue; }; }
+                else { if (d == 0) { Console.WriteLine("{0} - простое число", number); z++;  } else { Console.WriteLine("{0} - не простое", number); z++;} }
+
+                 number1 = number;
+
+
+            }
+                
+               
+            
+        }
+
+
+
+
+       public DefiningSimpleComplexNumber() {    }
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
+
+        
+
+
+
+
+
+
+
+
+
+       
+
+  
---
+++ CNN-AlgorithmsTask1/CNN-AlgorithmsTask1/TestCaseComplexNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CNN_AlgorithmsTask1
+{
+    class TestCaseComplexNumber
+    {
+        public double[] test1 = new double[] { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 }; // простые числа
+       
+       
+        public double[] test2 = new double[] { 1, 4, 6, 8, 12, 14, 18, 20, 22, 28, 30, 38, 40, 45, 49, 52, 57, 63, 68, 70, 73, 75, 82, 87, 96 }; // сложные числа
+       
+       
+        
+
+
+    
+        public TestCaseComplexNumber() { }
+
+
+
+
+
+
+
+    }
+}
